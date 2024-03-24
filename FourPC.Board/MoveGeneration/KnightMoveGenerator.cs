1a8077dee94bf184c276dfb7c6ec98aab96ef630
Astralfish <adrainstain@gmail.com>
2024-03-24 21:36:46 +0100

    #005 knight move generator

---
+++ FourPC.Board/MoveGeneration/KnightMoveGenerator.cs
@@ -0,0 +1,20 @@
+﻿using FourPC.Core.Chessboard.Cells;
+using FourPC.Core.Chessboard.Pieces;
+using FourPC.Core.Chessboard;
+
+namespace FourPC.Core.MoveGeneration;
+
+public class KnightMoveGenerator
+{
+    public static IEnumerable<Move> CastAlongAxes(Board board, Piece piece, int length = int.MaxValue)
+    {
+        return RayCaster.CastRay(board, piece, new CellPosition(1, 2), length)
+            .Concat(RayCaster.CastRay(board, piece, new CellPosition(-1, 2), length))
+            .Concat(RayCaster.CastRay(board, piece, new CellPosition(1, -2), length))
+            .Concat(RayCaster.CastRay(board, piece, new CellPosition(-1, -2), length))
+            .Concat(RayCaster.CastRay(board, piece, new CellPosition(2, 1), length))
+            .Concat(RayCaster.CastRay(board, piece, new CellPosition(2, -1), length))
+            .Concat(RayCaster.CastRay(board, piece, new CellPosition(-2, 1), length))
+            .Concat(RayCaster.CastRay(board, piece, new CellPosition(-2, -1), length));
+    }
+}
